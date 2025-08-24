50d997b9ec307490fe311f026d4b74a63856e383
SINGO Yao Dieu Donné <siruto@localhost.localdomain>
2025-08-24 11:57:40 +0000

    Resolution des divers probleme de deploiement'

--- Frontend/react-app-admin-car/src/components/inputs/Select.jsx
+++ Frontend/react-app-admin-car/src/components/inputs/Select.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react'
 
 class Select extends Component {
   render() {
+    const { name, label, value, onChange, required, options } = this.props;
+
     return (
-      <div>
-        
+      <div className="mb-4">
+        {label && (
+          <label htmlFor={name} className="block text-gray-700 font-medium mb-1">
+            {label}
+          </label>
+        )}
+        <select
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          required={required}
+          className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="">-- Sélectionnez --</option>
+          {options.map((option) => (
+            <option key={option.value} value={option.value} className='text-gray-700'>
+              {option.label}
+            </option>
+          ))}
+        </select>
       </div>
-    )
+    );
   }
 }
 
